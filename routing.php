4be96da8469e60e8087933b8ee1068c11b5326e1
Kovács Zsolt <smith.zsolt@gmail.com>
2016-11-07 01:13:26 +0100

    new routing

--- routing.php
+++ routing.php
@@ -4,7 +4,7 @@ namespace finger;
 use \finger\session as session;
 
 /**
- * Session handler
+ * Routing handler
  * @package finger
  */
 class routing
@@ -21,17 +21,32 @@ class routing
             $_session = new session();
             $_tmp = explode('/', $url);
             $_session->setValue('currentLang', $_tmp[1]);
-            header('Location: /');
+            header('Location: ' . $_session->getValue('prevURL', '/'));
             exit;
         }
         return $url;
     }
 
-    public static function find($url)
+    public static function find($url,&$param='')
     {
-        $url = self::detectLanguage($url);
+        //echo 'r';exit;
+        $_url = self::detectLanguage($url);
         $_configRouting = new config('routing');
-        $url=$_configRouting->get($url,$url);
+        //$url=$_configRouting->get($url,$url);
+        //return $url;
+        $_url = $_configRouting->get($url, NULL);
+        if (is_null($_url)) {
+            $_like=array();
+            foreach ($_configRouting->getAll() as $_routing_url=>$_routing) {
+                if (strpos($_routing_url,'/%')==true) {
+                    $_main_url=substr($_routing_url,0,-1);
+                    if (!(strpos($url,$_main_url)===false)) {
+                        $param=str_replace($_main_url,'',$url);
+                        return $_routing;
+                    }
+                }
+            }
+        }
         return $url;
     }
 }
