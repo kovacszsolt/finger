cb09ee0d3f6afd2e34d2bddae8a41ee0d24dbb07
Kovács Zsolt <smith.zsolt@gmail.com>
2016-11-08 22:07:23 +0100

    url protocol

--- request.php
+++ request.php
@@ -20,17 +20,61 @@ class request
      */
     public static function get($name, $default = '', $type = request::STRING)
     {
-        $_tmp_request=array_merge($_GET,$_POST);
+        $_tmp_request = array_merge($_GET, $_POST);
         $_return = $default;
         if (isset($_tmp_request[$name])) {
             $_return = $_tmp_request[$name];
         }
         switch ($type) {
             case request::NUMBER :
-                $_return = ($_return == '') ? 0 : $_return ;
+                $_return = ($_return == '') ? 0 : $_return;
                 break;
         }
         return $_return;
+    }
+
+    /**
+     * return Current URL
+     * @return mixed
+     */
+    public static function currentURL()
+    {
+        return $_SERVER['REQUEST_URI'];
+    }
+
+    /**
+     * Get Current URL with host name
+     * @return string
+     */
+    public static function currentFullURL()
+    {
+        return self::getProtocol() . '://' . self::getServerName() . self::currentURL();
+    }
+
+    /**
+     * Get Current Server Name
+     * @return mixed
+     */
+    public static function getServerName()
+    {
+        $servername = $_SERVER['SERVER_NAME'];
+        return $servername;
+    }
+
+    /**
+     * get Current Protocol
+     * @return string
+     */
+    public static function getProtocol()
+    {
+        $protocol = 'http';
+        if ((isset($_SERVER['HTTPS'])) && ($_SERVER['HTTPS'] == 'on')) {
+            $protocol = 'https';
+        }
+        if ((isset($_SERVER['SERVER_PORT'])) && ($_SERVER['SERVER_PORT'] == '443')) {
+            $protocol = 'https';
+        }
+        return $protocol;
     }
 
 }
