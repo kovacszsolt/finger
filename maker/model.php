7871f29d17f99edaa197d0104978c755ce8e7275
Kovács Zsolt <smith.zsolt@gmail.com>
2016-11-16 22:02:55 +0100

    create maker

--- maker/model.php
+++ maker/model.php
@@ -28,13 +28,23 @@ class model extends \finger\maker\main
     public function create($type, $name)
     {
         switch ($type) {
+            case 'full':
+                $this->createFile('model/content.table.template', array('modelname' => $name), 'app/model/' . $name . '/content/table.php');
+                $this->createFile('model/content.record.template', array('modelname' => $name), 'app/model/' . $name . '/content/record.php');
+
+                $this->createFile('model/language.table.template', array('modelname' => $name), 'app/model/' . $name . '/language/table.php');
+                $this->createFile('model/language.record.template', array('modelname' => $name), 'app/model/' . $name . '/language/record.php');
+
+                $this->createFile('model/image.table.template', array('modelname' => $name), 'app/model/' . $name . '/image/table.php');
+                $this->createFile('model/image.record.template', array('modelname' => $name), 'app/model/' . $name . '/image/record.php');
+                break;
             case 'contentrel':
-                $this->createFile('model.contentrel.table.template', array('modelname' => $name), 'app/model/' . $name . '/content/table.php');
-                $this->createFile('model.contentrel.record.template', array('modelname' => $name), 'app/model/' . $name . '/content/record.php');
+                $this->createFile('model/contentrel.table.template', array('modelname' => $name), 'app/model/' . $name . '/content/table.php');
+                $this->createFile('model/contentrel.record.template', array('modelname' => $name), 'app/model/' . $name . '/content/record.php');
                 break;
             default:
-                $this->createFile('model.' . $type . '.table.template', array('modelname' => $name), 'app/model/' . $name . '/' . $type . '/table.php');
-                $this->createFile('model.' . $type . '.record.template', array('modelname' => $name), 'app/model/' . $name . '/' . $type . '/record.php');
+                $this->createFile('model/' . $type . '.table.template', array('modelname' => $name), 'app/model/' . $name . '/' . $type . '/table.php');
+                $this->createFile('model/' . $type . '.record.template', array('modelname' => $name), 'app/model/' . $name . '/' . $type . '/record.php');
         }
         echo 'ok' . PHP_EOL;
         exit;
