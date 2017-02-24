592171e5020afabe826e38471cec5329d22cfde8
Kovács Zsolt <smith.zsolt@gmail.com>
2017-02-24 23:22:47 +0100

    add logger class add server class some change in request class

--- request.php
+++ request.php
@@ -6,143 +6,163 @@ namespace finger;
  */
 class request
 {
-    /**
-     * String type
-     */
-    const STRING = 'string';
 
-    /**
-     * Number tyoe
-     */
-    const NUMBER = 'number';
+	const METHOD_POST = 'post';
 
-    /**
-     * File type JPG
-     */
-    const FILE_TYPE_JPG = 'image/jpeg';
+	const METHOD_GET = 'get';
 
-    /**
-     * File type PNG
-     */
-    const FILE_TYPE_PNG = 'image/png';
+	/**
+	 * String type
+	 */
+	const STRING = 'string';
 
-    /**
-     * File type GIF
-     */
-    const FILE_TYPE_GIF = 'image/gif';
+	/**
+	 * Number tyoe
+	 */
+	const NUMBER = 'number';
 
-    public static function backPath($number)
-    {
-        $_return = '';
-        for ($i = 0; $i < $number; $i++) {
-            $_return .= DIRECTORY_SEPARATOR . '..';
-        }
-        $_return .= DIRECTORY_SEPARATOR;
-        return $_return;
-    }
+	/**
+	 * File type JPG
+	 */
+	const FILE_TYPE_JPG = 'image/jpeg';
 
-    /**
-     * $_FILES array
-     * check file mime type
-     * @param string $name
-     * @param bool $check
-     * @param null $allow
-     * @return null
-     */
-    public static function files($name, $check = false, $allow = NULL)
-    {
-        $_return = NULL;
-        if (isset($_FILES[$name])) {
-            $_files = $_FILES[$name];
-            $_return = $_files;
-            if ($check) {
-                foreach ($_files['tmp_name'] as $_file_id => $_file) {
-                    if (!is_null($allow)) {
-                        $_mime_content_type = mime_content_type($_files['tmp_name'][$_file_id]);
-                        if (!in_array($_mime_content_type, $allow)) {
-                            $_return = NULL;
-                        }
-                    }
-                }
-            }
-        }
-        return $_return;
+	/**
+	 * File type PNG
+	 */
+	const FILE_TYPE_PNG = 'image/png';
 
-    }
+	/**
+	 * File type GIF
+	 */
+	const FILE_TYPE_GIF = 'image/gif';
 
-    /**
-     * Get $_GET and $_POST parameter
-     * @param $name
-     * @param string $default
-     * @param string $type
-     * @return int|string
-     */
-    public static function get($name, $default = '', $type = request::STRING)
-    {
-        $_tmp_request = array_merge($_GET, $_POST);
-        $_return = $default;
-        if (isset($_tmp_request[$name])) {
-            $_return = $_tmp_request[$name];
-        }
-        switch ($type) {
-            case request::NUMBER :
-                $_return = ($_return == '') ? 0 : $_return;
-                break;
-        }
-        return $_return;
-    }
+	public static function backPath($number)
+	{
+		$_return = '';
+		for ($i = 0; $i < $number; $i++) {
+			$_return .= DIRECTORY_SEPARATOR . '..';
+		}
+		$_return .= DIRECTORY_SEPARATOR;
+		return $_return;
+	}
 
-    /**
-     * return Current URL
-     * @return mixed
-     */
-    public static function currentURL()
-    {
-        return $_SERVER['REQUEST_URI'];
-    }
+	/**
+	 * $_FILES array
+	 * check file mime type
+	 * @param string $name
+	 * @param bool $check
+	 * @param null $allow
+	 * @return null
+	 */
+	public static function files($name, $check = false, $allow = NULL)
+	{
+		$_return = NULL;
+		if (isset($_FILES[$name])) {
+			$_files = $_FILES[$name];
+			$_return = $_files;
+			if ($check) {
+				foreach ($_files['tmp_name'] as $_file_id => $_file) {
+					if (!is_null($allow)) {
+						$_mime_content_type = mime_content_type($_files['tmp_name'][$_file_id]);
+						if (!in_array($_mime_content_type, $allow)) {
+							$_return = NULL;
+						}
+					}
+				}
+			}
+		}
+		return $_return;
 
-    /**
-     * Get Current URL with host name
-     * @return string
-     */
-    public static function currentFullURL()
-    {
-        return self::getProtocol() . '://' . self::getServerName() . self::currentURL();
-    }
+	}
 
-    /**
-     * Get Current Server Name
-     * @return mixed
-     */
-    public static function getServerName()
-    {
-        $servername = $_SERVER['SERVER_NAME'];
-        return $servername;
-    }
+	/**
+	 * Set GET or POST parameter
+	 * @param $name name of key
+	 * @param $value
+	 * @param string $method GET/POST
+	 */
+	public static function set($name, $value, $method = request::METHOD_GET)
+	{
+		if ($method == request::METHOD_GET) {
+			$_GET[$name] = $value;
+		} elseif ($method == request::METHOD_POST) {
+			$_POST[$name] = $value;
+		}
+	}
 
-    /**
-     * Get current client IP Address
-     * @return string
-     */
-    public static function getClientIPAddress()
-    {
-        return $_SERVER['REMOTE_ADDR'];
-    }
+	/**
+	 * Get $_GET and $_POST parameter
+	 * @param $name
+	 * @param string $default
+	 * @param string $type
+	 * @return int|string
+	 */
+	public static function get($name, $default = '', $type = request::STRING)
+	{
+		$_tmp_request = array_merge($_GET, $_POST);
+		$_return = $default;
+		if (isset($_tmp_request[$name])) {
+			$_return = $_tmp_request[$name];
+		}
+		switch ($type) {
+			case request::NUMBER :
+				$_return = ($_return == '') ? 0 : $_return;
+				break;
+		}
+		return $_return;
+	}
 
-    /**
-     * get Current Protocol
-     * @return string
-     */
-    public static function getProtocol()
-    {
-        $protocol = 'http';
-        if ((isset($_SERVER['HTTPS'])) && ($_SERVER['HTTPS'] == 'on')) {
-            $protocol = 'https';
-        }
-        if ((isset($_SERVER['SERVER_PORT'])) && ($_SERVER['SERVER_PORT'] == '443')) {
-            $protocol = 'https';
-        }
-        return $protocol;
-    }
+	/**
+	 * return Current URL
+	 * @return mixed
+	 */
+	public static function currentURL()
+	{
+		return $_SERVER['REQUEST_URI'];
+	}
+
+	/**
+	 * Get Current URL with host name
+	 * @return string
+	 */
+	public static function currentFullURL()
+	{
+		return self::getProtocol() . '://' . self::getServerName() . self::currentURL();
+	}
+
+	/**
+	 * Get Current Server Name
+	 * @return mixed
+	 */
+	public static function getServerName()
+	{
+		$servername = $_SERVER['SERVER_NAME'];
+		return $servername;
+	}
+
+	/**
+	 * Get current client IP Address
+	 * @return string
+	 */
+	public static function getClientIPAddress()
+	{
+		return $_SERVER['REMOTE_ADDR'];
+	}
+
+	/**
+	 * get Current Protocol
+	 * @return string
+	 */
+	public static function getProtocol()
+	{
+		$protocol = 'http';
+		if ((isset($_SERVER['HTTPS'])) && ($_SERVER['HTTPS'] == 'on')) {
+			$protocol = 'https';
+		}
+		if ((isset($_SERVER['SERVER_PORT'])) && ($_SERVER['SERVER_PORT'] == '443')) {
+			$protocol = 'https';
+		}
+		return $protocol;
+	}
 
 }
