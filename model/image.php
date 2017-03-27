7bcc37cac6129da2a6c84bc4d72b6b10591a72e9
Kovács Zsolt <kovacs@greenroom.hu>
2017-03-27 10:12:49 +0200

    file size

--- model/image.php
+++ model/image.php
@@ -11,49 +11,50 @@ use \finger\storage as storage;
 class image extends \finger\database\main
 {
 
-    /**
-     * Database fields
-     * @var array
-     */
-    public $fields = array(
-        'rootid' => array('type' => 'int(10)','update'=>false),
-        'name' => array('type' => 'varchar(200)','update'=>true),
-        'alt' => array('type' => 'varchar(200)','update'=>true),
-        'extension' => array('type' => 'varchar(200)','update'=>false),
-	    'size' => array('type' => 'int(10)','update'=>true),
-    );
+	/**
+	 * Database fields
+	 * @var array
+	 */
+	public $fields = array(
+		'rootid' => array('type' => 'int(10)', 'update' => false),
+		'name' => array('type' => 'varchar(200)', 'update' => true),
+		'alt' => array('type' => 'varchar(200)', 'update' => true),
+		'extension' => array('type' => 'varchar(200)', 'update' => false),
+		'size' => array('type' => 'int(10)', 'update' => true),
+	);
 
-    /**
-     * image path
-     * @var
-     */
-    protected $path;
+	/**
+	 * image path
+	 * @var
+	 */
+	protected $path;
 
-    /**
-     * Save record to database and save file to storege dir
-     * @param $record
-     */
-    public function add($record)
-    {
+	/**
+	 * Save record to database and save file to storege dir
+	 * @param $record
+	 */
+	public function add($record)
+	{
+		//Get File size
+		$record->setSize(storage::sizeFile($record->getTmpFileName()));
+		$_id = parent::add($record);
+		storage::saveFile($record->getTmpFileName(), $this->path . DIRECTORY_SEPARATOR . $_id . '.' . $record->getExtension(), true);
 
-        $_id = parent::add($record);
-        storage::saveFile($record->getTmpFileName(), $this->path . DIRECTORY_SEPARATOR . $_id . '.' . $record->getExtension(), true);
+	}
 
-    }
+	/**
+	 * Delete record from database and remove file from storage
+	 * @param $id
+	 * @return bool
+	 */
+	public function delete($id)
+	{
+		$_record = $this->find($id);
+		//print_r($_record);exit;
+		storage::removeFile($this->path . DIRECTORY_SEPARATOR . $_record->getID() . '.' . $_record->getExtension());
 
-    /**
-     * Delete record from database and remove file from storage
-     * @param $id
-     * @return bool
-     */
-    public function delete($id)
-    {
-        $_record = $this->find($id);
-        //print_r($_record);exit;
-        storage::removeFile($this->path . DIRECTORY_SEPARATOR . $_record->getID() . '.' . $_record->getExtension());
-
-        return parent::delete($id);
-    }
+		return parent::delete($id);
+	}
 
 
 }
