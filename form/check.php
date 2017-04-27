ee69242e5a645bccda0cc7d4d45581422e93fbb4
Kovács Zsolt <smith.zsolt@gmail.com>
2017-04-27 10:14:11 +0200

    form validation step 1

--- form/check.php
+++ form/check.php
@@ -9,54 +9,73 @@ use \finger\form\field as formfield;
 
 class check
 {
-	private $fields;
+    private $fields;
 
-	public function addField($name, $required, $type, $length = '')
-	{
-		$this->fields[$name] = new formfield($name, $required, $type, $length);
-	}
+    public function addField($name, $required, $type, $length = '')
+    {
+        $this->fields[$name] = new formfield($name, $required, $type, $length);
+    }
 
-	public static function generateKey()
-	{
-		$_session = new session();
-		$_key = random::char(10);
-		$_value = random::char(10);
-		$_return = $_key . '.' . $_value;
-		$_session->setValue($_key, $_value);
-		return $_return;
-	}
+    public static function generateKey()
+    {
+        $_session = new session();
+        $_key = random::char(10);
+        $_value = random::char(10);
+        $_return = $_key . '.' . $_value;
+        $_session->setValue($_key, $_value);
+        return $_return;
+    }
 
-	public function init($filename)
-	{
-		$_return = false;
-		$_filename = MAINPATH . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'form' . DIRECTORY_SEPARATOR . \finger\server::host() . DIRECTORY_SEPARATOR . $filename . '.ini';
-		if (is_file($_filename)) {
-			$_data = parse_ini_file($_filename, true);
-			foreach ($_data as $key => $value) {
-				$_value_array = explode('.', $value);
-				$this->addField($key, $_value_array[0], $_value_array[1], (isset($_value_array[1])) ? 0 : $_value_array[1]);
-			}
-			$this->addField('timestampkey', 'timestampkey');
-			$_return = true;
-		}
-		return $_return;
-	}
-
-	public function check()
-	{
-		$_return = false;
-		$_request = request::getPOST();
-		$_key_compare = array_merge(array_diff_key($_request, $this->fields), array_diff_key($this->fields, $_request));
-		if (sizeof($_key_compare) == 0) {
-			foreach ($this->fields as $field_name => $field) {
-				$field->setValue($_request[$field_name]);
-			}
-			$_timestampkey=request::get('timestampkey','');
-			if (sizeof(explode('.',$_timestampkey))==2) {
-
+    public function init($filename)
+    {
+        $_return = false;
+        $_filename = MAINPATH . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'form' . DIRECTORY_SEPARATOR . \finger\server::host() . DIRECTORY_SEPARATOR . $filename . '.ini';
+        if (is_file($_filename)) {
+            $_data = parse_ini_file($_filename, true);
+            foreach ($_data as $key => $value) {
+                $_value_array = explode('.', $value);
+                $this->addField($key, $_value_array[0], $_value_array[1], (isset($_value_array[1])) ? 0 : $_value_array[1]);
             }
-			print_r($_SESSION);exit;
-		}
+            $_return = true;
+        }
+        return $_return;
+    }
 
-	}
+    private function checkFields()
+    {
+        $_return = false;
+        foreach ($this->fields as $field_name => $field) {
+            if ($field->checkRequired()) {
+                $field->check();
+            }
+        }
+        echo 'q';
+        exit;
+        return $_return;
+    }
+
+    public function check()
+    {
+        $_return = false;
+        $_request = request::getPOST();
+        $_key_compare = array_merge(array_diff_key($_request, $this->fields), array_diff_key($this->fields, $_request));
+        if (sizeof($_key_compare) == 0) {
+            foreach ($this->fields as $field_name => $field) {
+                $field->setValue($_request[$field_name]);
+            }
+            $this->checkFields();
+
+
+            print_r($this->fields);
+            exit;
+            $_timestampkey = request::get('timestampkey', '');
+            if (sizeof(explode('.', $_timestampkey)) == 2) {
+                echo 'timestamp';
+                exit;
+            }
+            print_r($_SESSION);
+            exit;
+        }
+
+    }
 }
--- form/field.php
+++ form/field.php
@@ -3,53 +3,136 @@
 namespace finger\form;
 
 use \finger\session as session;
+use \finger\request as request;
 use \finger\random as random;
 
 class field
 {
-	private $name;
-	private $required;
-	private $type;
-	private $length;
-	private $value;
+    private $name;
+    private $required;
+    private $type;
+    private $length;
+    private $value;
+    private $_session;
 
-	/**
-	 * field constructor.
-	 * @param string $name
-	 * @param string $required
-	 * @param string $type
-	 * @param string $length
-	 */
-	public function __construct($name, $required, $type, $length = '')
-	{
-		$this->setName($name);
-		$this->setRequired($required);
-		$this->setType($type);
-		$this->setLength($length);
-	}
+    /**
+     * field constructor.
+     * @param string $name
+     * @param string $required
+     * @param string $type
+     * @param string $length
+     */
+    public function __construct($name, $required, $type, $length = '')
+    {
+        $this->_session = new session();
+        $this->setName($name);
+        $this->setRequired($required);
+        $this->setType($type);
+        $this->setLength($length);
+    }
 
-	public function setName($value)
-	{
-		$this->name = $value;
-	}
+    /**
+     * Set Field name
+     * @param $value
+     */
+    public function setName($value)
+    {
+        $this->name = $value;
+    }
 
-	public function setRequired($value)
-	{
-		$this->required = $value;
-	}
+    /**
+     * Set required state
+     * @param $value
+     */
+    public function setRequired($value)
+    {
+        $this->required = $value;
+    }
 
-	public function setType($value)
-	{
-		$this->type = $value;
-	}
+    /**
+     * Set field type
+     * @param $value
+     */
+    public function setType($value)
+    {
+        $this->type = $value;
+    }
 
-	public function setLength($value)
-	{
-		$this->length = $value;
-	}
+    /**
+     * Set field min lenght
+     * @param $value
+     */
+    public function setLength($value)
+    {
+        $this->length = $value;
+    }
 
-	public function setValue($value)
-	{
-		$this->value = $value;
-	}
+    /**
+     * Set field Value
+     * @param $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * Check field
+     * @return bool
+     */
+    public function check()
+    {
+        $_return = false;
+        switch ($this->type) {
+            case 'email' :
+                $_return = $this->checkEmail();
+                break;
+            case 'csrf':
+                $_return = $this->checkCSRF();
+            default:
+                echo 'no: ' . $this->type;
+                exit;
+
+        }
+        return $_return;
+    }
+
+    /**
+     * Check valid email
+     * @return bool
+     */
+    private function checkEmail()
+    {
+        $_return = ($this->value == filter_var($this->value, FILTER_VALIDATE_EMAIL)) ? true : false;
+        return $_return;
+    }
+
+    public function checkRequired()
+    {
+        $_return = false;
+        if ($this->required == 'req') {
+            $_return = true;
+        }
+        return $_return;
+    }
+
+    /**
+     * Check CSRF Token
+     * @return bool
+     */
+    private function checkCSRF()
+    {
+        $_return = false;
+        $_valueArray = explode('.', $this->value);
+        if (sizeof($_valueArray) == 2) {
+            $_keyPre = $this->_session->getValue($_valueArray[0], '');
+            if ($_keyPre != '') {
+                $this->_session->remove($_valueArray[0]);
+                if ($_keyPre == $_valueArray[1]) {
+                    $_return = true;
+                }
+            }
+        }
+        return $_return;
+    }
 }
