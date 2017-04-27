54f2e2c3ea8f1a8722be95737d6258a620a6bb72
Kovács Zsolt <smith.zsolt@gmail.com>
2017-04-27 13:41:37 +0200

    add mail class

---
+++ mail/mail.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace finger\mail;
+
+use finger\config as config;
+
+/**
+ * Class mail
+ * use Swift mailer
+ * @package finger\mail
+ */
+class mail
+{
+
+    private $subject = '';
+    private $from = array();
+    private $body = '';
+    private $to = array();
+    private $_transport;
+
+    /**
+     * mail constructor.
+     * read mail settings from config
+     */
+    public function __construct()
+    {
+        $_config = new config('mail');
+        $this->_transport = \Swift_SmtpTransport::newInstance($_config->get('server'), 25);
+        $this->_transport->setUsername($_config->get('username'));
+        $this->_transport->setPassword($_config->get('password'));
+    }
+
+    /**
+     * Set mail From
+     * @param string $email
+     * @param string $name
+     */
+    public function setFrom($email, $name = '')
+    {
+        $this->from = array($email => ($name == '') ? $email : $name);
+    }
+
+    /**
+     * Set mail subject
+     * @param string $value
+     */
+    public function setSubject($value)
+    {
+        $this->subject = $value;
+    }
+
+    /**
+     * Set mail body
+     * @param string $value
+     */
+    public function setBody($value)
+    {
+        $this->body = $value;
+    }
+
+    /**
+     * Add mail address
+     * @param string $email
+     * @param string $name
+     */
+    public function addTo($email, $name = '')
+    {
+        $this->to[] = array($email, ($name == '') ? $email : $name);
+    }
+
+    /**
+     * Send mail
+     */
+    public function send()
+    {
+        $_message = \Swift_Message::newInstance();
+        $_message->setSubject($this->subject);
+        $_message->setFrom($this->from);
+        $_message->setTo($this->to);
+        $_message->setBody($this->body);
+        $_mailer = \Swift_Mailer::newInstance($this->_transport);
+        $result = $_mailer->send($_message);
+    }
+
+}
